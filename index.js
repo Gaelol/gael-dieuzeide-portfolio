4fb34fdea2a4e5ab3fc5c195e9da43c361e364c8
GaelDieuz <gael-dieuzeide@hotmail.com>
2021-05-15 17:12:17 +0200

    Structure Portfolio

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-const cool = require('cool-ascii-faces');
 const express = require('express');
+const app = express();
 const path = require('path');
-const PORT = process.env.PORT || 5000;
+const router = express.Router();
 
-express()
-    .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => res.sendFile('index.html'))
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+router.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
+    //__dirname : It will resolve to your project folder.
+});
+
+router.get('/presentation',function(req,res){
+    res.sendFile(path.join(__dirname+'/presentation.html'));
+});
+
+router.get('/projet',function(req,res){
+    res.sendFile(path.join(__dirname+'/projet.html'));
+});
+router.get('/compétences',function(req,res){
+    res.sendFile(path.join(__dirname+'/competences.html'));
+});
+router.get('/cv',function(req,res){
+    res.sendFile(path.join(__dirname+'/cv.html'));
+});
+router.get('/activites',function(req,res){
+    res.sendFile(path.join(__dirname+'/activites.html'));
+});
+router.get('/portfolio',function(req,res){
+    res.sendFile(path.join(__dirname+'/portfolio.html'));
+});
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 5000);
