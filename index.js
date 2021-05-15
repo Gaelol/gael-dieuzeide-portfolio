453953afff7e841f4521d05d943e6bf656defa27
GaelDieuz <gael-dieuzeide@hotmail.com>
2021-05-15 16:46:20 +0200

    Add cool face API

--- index.js
+++ index.js
@@ -8,5 +8,4 @@ express()
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs')
     .get('/', (req, res) => res.sendFile('index.html'))
-    .get('/cool', (req, res) => res.send(cool()))
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
